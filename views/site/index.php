4f305e6bafd2769749d60cb4dbac131c1e68acf9
MishanyaGG <mischatitovmail.ru@gmail.com>
2024-05-07 23:35:09 +0300

    Создал первый список на главной странице

--- views/site/index.php
+++ views/site/index.php
@@ -4,50 +4,30 @@
 
 $this->title = 'My Yii Application';
 ?>
-<div class="site-index">
-
-    <div class="jumbotron text-center bg-transparent mt-5 mb-5">
-        <h1 class="display-4">Congratulations!</h1>
-
-        <p class="lead">You have successfully created your Yii-powered application.</p>
-
-        <p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
+<h3>Товары</h3>
+<div class="list_tovar" style="display: flex">
+    <div class="coll">
+        <img src="<?= \yii\helpers\Url::to('img/Krovati.svg') ?>" alt="Кровати" style="border-radius: 10px">
+        <h5>Кровати</h5>
     </div>
 
-    <div class="body-content">
+    <div class="coll">
+        <img src="<?= \yii\helpers\Url::to('img/Krovati.svg') ?>" alt="Кровати" style="border-radius: 10px">
+        <h5>Кровати</h5>
+    </div>
 
-        <div class="row">
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
+    <div class="coll">
+        <img src="<?= \yii\helpers\Url::to('img/Krovati.svg') ?>" alt="Кровати" style="border-radius: 10px">
+        <h5>Кровати</h5>
+    </div>
 
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
+    <div class="coll">
+        <img src="<?= \yii\helpers\Url::to('img/Krovati.svg') ?>" alt="Кровати" style="border-radius: 10px">
+        <h5>Кровати</h5>
+    </div>
 
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/doc/">Yii Documentation &raquo;</a></p>
-            </div>
-            <div class="col-lg-4 mb-3">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/forum/">Yii Forum &raquo;</a></p>
-            </div>
-            <div class="col-lg-4">
-                <h2>Heading</h2>
-
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                    dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip
-                    ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
-                    fugiat nulla pariatur.</p>
-
-                <p><a class="btn btn-outline-secondary" href="https://www.yiiframework.com/extensions/">Yii Extensions &raquo;</a></p>
-            </div>
-        </div>
-
+    <div class="coll">
+        <img src="<?= \yii\helpers\Url::to('img/Krovati.svg') ?>" alt="Кровати" style="border-radius: 10px">
+        <h5>Кровати</h5>
     </div>
 </div>
