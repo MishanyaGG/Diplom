cbd472bc21f025036478b589b281a20219d74862
MishanyaGG <mischatitovmail.ru@gmail.com>
2024-05-07 23:20:00 +0300

    Сделл подвал

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -29,14 +29,13 @@ AppAsset::register($this);
       <div class="col-md-3 mb-2 mb-md-0">
         <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
             <img width="80" height="80" src="<?= \yii\helpers\Url::to('img/logo.svg') ?>" alt="logo">
-<!--          <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap"><use ></use></svg>-->
         </a>
       </div>
 
       <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
         <li><a href="#" class="nav-link px-2" style="border-bottom: 1px solid black; height: 35px; color: black">Товары</a></li>
-        <li><a href="#" class="nav-link px-2">Контакты</a></li>
-        <li><a href="#" class="nav-link px-2">Конструктор</a></li>
+        <li><a href="#" class="nav-link px-2" style="height: 35px; color: black">Контакты</a></li>
+        <li><a href="#" class="nav-link px-2" style="height: 35px; color: black">Конструктор</a></li>
       </ul>
 
       <div class="col-md-3 text-end">
@@ -55,10 +54,42 @@ AppAsset::register($this);
 
 <footer id="footer" class="mt-auto py-3 bg-light">
     <div class="container">
-        <div class="row text-muted">
-            <div class="col-md-6 text-center text-md-start">&copy; My Company <?= date('Y') ?></div>
-            <div class="col-md-6 text-center text-md-end"><?= Yii::powered() ?></div>
+        <div style="display: flex">
+            <div class="left_footer">
+                <h3>Каталог</h3>
+                <div class="list_coll" style="display: flex">
+                    <div class="coll">
+                        <p>Спальня</p>
+                        <p>Гостиная</p>
+                        <p>Прихожая</p>
+                    </div>
+
+                    <div class="coll">
+                        <p>Молодежная</p>
+                        <p>Кабинет</p>
+                        <p>Кухня</p>
+                    </div>
+                </div>
+            </div>
+            <div class="right_footer">
+                <h3>Контакты</h3>
+                <div class="list_coll" style="display: flex">
+                    <div class="coll">
+                        <p>Спальня</p>
+                        <p>Гостиная</p>
+                        <p>Прихожая</p>
+                    </div>
+                </div>
+            </div>
         </div>
+        <div style="display: flex; background: #E9E9E9; width: 100%; padding: 20px;">
+            <i style="margin-left: 20%">Нужна помощь?</i>
+            <p style="margin-left: 10%"><b>Телефон:</b> 8(800) 555 03 11</p>
+            <p style="margin-left: 10%"><b>E-mail:</b> <a href="mailto:mishatitovmail.ru@gmail.com">mishatitovmail.ru@gmail.com</a></p>
+        </div>
+        <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+            <img width="80" height="80" src="<?= \yii\helpers\Url::to('img/logo.svg') ?>" alt="logo">
+        </a>
     </div>
 </footer>
 
